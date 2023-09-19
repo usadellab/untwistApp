04d5a0bcd013ed38a2ffdede664e8b2f59fc5371
ataulhaleem <ataulhaleem@gmail.com>
2023-09-19 15:59:27 +0200

    obj list

--- contexts/ObjListContext.js
+++ contexts/ObjListContext.js
@@ -3,7 +3,7 @@ import React, { createContext, useContext, useState, useEffect } from 'react';
 import axios from 'axios';
 import { useTokenContext } from './TokenContext';
 
-const apiEndpoint = 'http://134.94.65.182:5000' // 'http://127.0.0.1:10000'
+const apiEndpoint = 'default end point ' 
 
 const ObjListContext = createContext();
 
