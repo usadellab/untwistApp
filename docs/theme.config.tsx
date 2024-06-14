74559a34ecad7a99333b58e4400ab894ea4296e0
ataulhaleem <ataulhaleem@gmail.com>
2024-06-14 07:50:14 +0200

    contact info

--- docs/theme.config.tsx
+++ docs/theme.config.tsx
@@ -4,14 +4,14 @@ import { DocsThemeConfig } from 'nextra-theme-docs'
 const config: DocsThemeConfig = {
   logo: <span>Camelina-hub documentation</span>,
   project: {
-    // link: 'https://github.com/shuding/nextra-docs-template',
+    link: 'https://github.com/usadellab/untwistApp',
   },
   chat: {
     // link: 'https://discord.com',
   },
-  docsRepositoryBase: 'https://github.com/shuding/nextra-docs-template',
+  docsRepositoryBase: 'https://github.com/usadellab/untwistApp/tree/documentation',
   footer: {
-    text: 'Nextra Docs Template',
+    text: 'Camelina-hub documentation',
   },
 }
 
