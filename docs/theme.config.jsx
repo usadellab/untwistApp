2ae06a1cd8cc2878baa3eeccc5a46433e885cf60
ataulhaleem <ataulhaleem@gmail.com>
2024-06-28 17:02:41 +0200

    update:docs

--- docs/theme.config.jsx
+++ docs/theme.config.jsx
@@ -3,7 +3,15 @@ import { DocsThemeConfig } from 'nextra-theme-docs'
 import { useRouter } from 'next/router'
 
 export default  {
-  logo: <span>Camelina-hub documentation</span>,
+  // logo: <span>Camelina-hub documentation</span>,
+    logo: (
+      <>
+<img src='/FAV-32.png' alt="Hello" width={20} height={20} />
+          <span style={{marginLeft : '0.3em', fontWeight: 1000, fontSize : 25 }}>
+          Camelina-hub documentation
+        </span>
+      </>
+    ),
   project: {
     link: 'https://github.com/usadellab/untwistApp',
   },
@@ -12,7 +20,7 @@ export default  {
     const { asPath } = useRouter()
     if (asPath !== '/') {
       return {
-        titleTemplate: '%s '
+        titleTemplate: '%s '  //replaced the default 'Nextra' text with empty string
       }
     }
   },
